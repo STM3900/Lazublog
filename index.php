123fdb719d955d543f1b9875c318d2baa329a4e1
Théo <theo_38180@live.fr>
2020-03-08 02:49:27 +0100

    Création de la page de base

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Overlock&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>BlogAC</title>
+</head>
+<body>
+    <header>
+        <aside></aside>
+        <h1>Bienvenue sur mon blog !</h1>
+    </header>
+    <div class="general-content">
+        <aside></aside>
+        <section class="desc">
+        <p>Vous aimez les petites histoires ? Je vous propose dès la sortie d'Animal Crossing : New Horizon, de vivre
+            mon aventure avec moi, à travers des petites articles que je posterais !</p>
+        </section>
+        <div class="timeline">
+            <div class="container left">
+                <div class="content">
+                    <aside></aside>
+                    <h2>Jour 0</h2>
+                    <p>Le grand départ !</p>
+                </div>
+            </div>
+            <div class="container right">
+                <div class="content">
+                    <aside></aside>
+                    <h2>Jour 1</h2>
+                    <p>La découverte de l'île</p>
+                </div>
+            </div>
+            <div class="container left">
+                <div class="content">
+                    <aside></aside>
+                    <h2>Jour 2</h2>
+                    <p>La grande exploration</p>
+                </div>
+            </div>
+            <div class="container right">
+                <div class="content">
+                    <aside></aside>
+                    <h2>Jour 3</h2>
+                    <p>La mystérieuse rencontre</p>
+                </div>
+            </div>
+            <div class="container left">
+                <div class="content">
+                    <aside></aside>
+                    <h2>Jour 4</h2>
+                    <p>La chasse au coquillages</p>
+                </div>
+            </div>
+            <div class="container right">
+                <div class="content">
+                    <aside></aside>
+                    <h2>Jour 5</h2>
+                    <p>Le concert de KK</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
